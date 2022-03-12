16bf7f02e92cec0f8a3e530910c6f0ae0173ea8e
Thomas Patzke <thomas@patzke.org>
2022-03-12 11:02:58 +0100

    Rule check command

--- sigma/cli/check.py
+++ sigma/cli/check.py
@@ -1,6 +1,31 @@
+import pathlib
 import click
 
+from sigma.collection import SigmaCollection
+from sigma.exceptions import SigmaError
+
 @click.command()
-def check():
+@click.option(
+    "--file-pattern", "-P",
+    default="*.yml",
+    show_default=True,
+    help="Pattern for file names to be included in recursion into directories.",
+)
+@click.argument(
+    "input",
+    nargs=-1,
+    type=click.Path(exists=True, path_type=pathlib.Path),
+)
+def check(input, file_pattern):
     """Check Sigma rules for validity and best practices (not yet implemented)."""
-    pass
+    try:
+        rule_collection = SigmaCollection.load_ruleset(
+            input,
+            recursion_pattern="**/" + file_pattern,
+            collect_errors=True,
+            )
+        for rule in rule_collection.rules:
+            for error in rule.errors:
+                click.echo(error)
+    except SigmaError as e:
+        click.echo("Check error: " + str(e), err=True)
