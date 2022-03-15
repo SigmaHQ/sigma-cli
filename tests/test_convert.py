a01a3c9cff796eb5038057d11251d349c2cb4295
Thomas Patzke <thomas@patzke.org>
2022-03-15 09:19:29 +0100

    Updated dependencies and propagated pySigma Splunk backend fix

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -4,12 +4,12 @@ from sigma.cli.convert import convert
 def test_convert_output_list():
     cli = CliRunner()
     result = cli.invoke(convert, ["-t", "splunk", "-p", "sysmon", "tests/files"])
-    assert 'EventID=1 ParentImage="*\httpd.exe" Image="*\cmd.exe"' in result.stdout
+    assert 'EventID=1 ParentImage="*\\\\httpd.exe" Image="*\\\\cmd.exe"' in result.stdout
 
 def test_convert_output_str():
     cli = CliRunner()
     result = cli.invoke(convert, ["-t", "splunk", "-p", "sysmon", "-f", "savedsearches", "tests/files"])
-    assert 'EventID=1 ParentImage="*\httpd.exe" Image="*\cmd.exe"' in result.stdout
+    assert 'EventID=1 ParentImage="*\\\\httpd.exe" Image="*\\\\cmd.exe"' in result.stdout
 
 def test_convert_unknown_format():
     cli = CliRunner()
