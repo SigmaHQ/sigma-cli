d103c40d059c8fe020d1b50e4cca9ead873d0f02
Thomas Patzke <thomas@patzke.org>
2022-03-22 00:03:38 +0100

    Updates dependencies and added new pipelines

--- sigma/cli/pipelines.py
+++ sigma/cli/pipelines.py
@@ -1,8 +1,11 @@
 from sigma.pipelines.sysmon import sysmon_pipeline
 from sigma.pipelines.crowdstrike import crowdstrike_fdr_pipeline
+from sigma.pipelines.splunk import splunk_windows_pipeline, splunk_windows_sysmon_acceleration_keywords
 from sigma.processing.resolver import ProcessingPipelineResolver
 
 pipelines = ProcessingPipelineResolver({
     "sysmon": sysmon_pipeline,
     "crowdstrike_fdr": crowdstrike_fdr_pipeline,
+    "splunk_windows": splunk_windows_pipeline,
+    "splunk_sysmon_acceleration": splunk_windows_sysmon_acceleration_keywords,
 })
