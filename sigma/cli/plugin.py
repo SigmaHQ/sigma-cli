2825346b40fd8bb6c7a81e6eac6e5f3e22f18fbc
Thomas Patzke <thomas@patzke.org>
2023-01-22 01:34:28 +0100

    Plugin search

--- sigma/cli/plugin.py
+++ sigma/cli/plugin.py
@@ -10,7 +10,8 @@ def plugin_group():
 @click.option("--plugin-type", "-t", type=click.Choice([ str(item) for item in SigmaPluginType ], case_sensitive=False), help="Display only plugins of given type.")
 @click.option("--plugin-state", "-s", type=click.Choice([ str(item) for item in SigmaPluginState ], case_sensitive=False), help="Display only plugins with given state.")
 @click.option("--compatible", "-c", is_flag=True, help="Show only plugins that are compatible with the installed pySigma version.")
-def list_plugins(plugin_type : str, plugin_state : str, compatible : bool):
+@click.argument("search", default="")
+def list_plugins(plugin_type : str, plugin_state : str, compatible : bool, search : str):
     plugins = SigmaPluginDirectory.default_plugin_directory()
     compatibility_state = {
         True: "yes",
@@ -23,12 +24,14 @@ def list_plugins(plugin_type : str, plugin_state : str, compatible : bool):
         get_plugins_args["plugin_types"] = { SigmaPluginType[ plugin_type.upper() ]}
     if plugin_state:
         get_plugins_args["plugin_states"] = { SigmaPluginState[ plugin_state.upper() ]}
+    norm_search = search.lower()
 
     table = PrettyTable()
     table.field_names = ("Identifier", "Type", "State", "Description", "Compatible?")
     table.add_rows([
         (plugin.id, str(plugin.type), str(plugin.state), plugin.description, compatibility_state[plugin.is_compatible()])
         for plugin in plugins.get_plugins(compatible_only=compatible, **get_plugins_args)
+        if norm_search in plugin.id.lower() or norm_search in plugin.description.lower()
     ])
     table.align = "l"
     click.echo(table.get_string())
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -15,3 +15,11 @@ def test_plugin_list_filtered():
     assert plugin_list.exit_code == 0
     assert plugin_list_filtered.exit_code == 0
     assert len(plugin_list.output.split()) > len(plugin_list_filtered.output.split())
+
+def test_plugin_list_search():
+    cli = CliRunner()
+    plugin_list = cli.invoke(list_plugins)
+    plugin_list_search = cli.invoke(list_plugins, [ "Sysmon" ])
+    assert plugin_list.exit_code == 0
+    assert plugin_list_search.exit_code == 0
+    assert len(plugin_list.output.split()) > len(plugin_list_search.output.split())
