651cdc300b052892bc79bf4ca936759c3aab67cf
Thomas Patzke <thomas@patzke.org>
2023-05-13 13:55:22 +0200

    Command "version"

--- sigma/cli/main.py
+++ sigma/cli/main.py
@@ -1,5 +1,6 @@
 import imp
 import click
+import pkg_resources
 from .list import list_group
 from .convert import convert
 from .check import check
@@ -10,12 +11,18 @@ from .analyze import analyze_group
 def cli():
     pass
 
+@click.command()
+def version():
+    """Print version of Sigma CLI."""
+    click.echo(pkg_resources.get_distribution("sigma-cli").version)
+
 def main():
     cli.add_command(analyze_group)
     cli.add_command(plugin_group)
     cli.add_command(list_group)
     cli.add_command(convert)
     cli.add_command(check)
+    cli.add_command(version)
     cli()
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,15 @@
-from sigma.cli.main import cli as main
+from sigma.cli.main import cli as main, version
 from click.testing import CliRunner
+import re
 
 def test_help():
     cli = CliRunner()
     result = cli.invoke(main, ["--help"])
     assert result.exit_code == 0
     assert len(result.stdout.split()) > 10
+
+def test_version():
+    cli = CliRunner()
+    result = cli.invoke(version)
+    assert result.exit_code == 0
+    assert re.search("\\d+\\.\\d+\\.\\d+", result.stdout)
